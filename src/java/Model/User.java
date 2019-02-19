336a6084a9c98478b8d0888f8a53b60418ead0ba
Matilda Sjöblom <matilda.sjo96@gmail.com>
2019-02-19 15:43:20 +0100

    revert to kevins commit

---
+++ src/java/Model/User.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.*;
+
+/**
+ *
+ * @author rackarmattan
+ */
+public class User {
+
+    //POSTGRESQL DATABASE
+    static final String DATABASE = "jdbc:postgresql://localhost/local";
+    static final String USERNAME = "rackarmattan";
+    static final String PASSWORD = "hejhej123";
+
+}
