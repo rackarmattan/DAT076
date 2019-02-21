1ba12594898d050af6f45520b35f148b3c67fa69
Matilda Sjöblom <matilda.sjo96@gmail.com>
2019-02-21 15:15:14 +0100

    added new database files

---
+++ src/java/com/project2/model/Accounts.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project2.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author rackarmattan
+ */
+@Entity
+@Table(name = "ACCOUNTS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Accounts.findAll", query = "SELECT a FROM Accounts a")
+    , @NamedQuery(name = "Accounts.findByLogin", query = "SELECT a FROM Accounts a WHERE a.login = :login")
+    , @NamedQuery(name = "Accounts.findByPassword", query = "SELECT a FROM Accounts a WHERE a.password = :password")
+    , @NamedQuery(name = "Accounts.findByEmail", query = "SELECT a FROM Accounts a WHERE a.email = :email")
+    , @NamedQuery(name = "Accounts.findByAbout", query = "SELECT a FROM Accounts a WHERE a.about = :about")})
+public class Accounts implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "LOGIN")
+    private String login;
+    @Basic(optional = false)
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "ABOUT")
+    private String about;
+
+    public Accounts() {
+    }
+
+    public Accounts(String login) {
+        this.login = login;
+    }
+
+    public Accounts(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (login != null ? login.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Accounts)) {
+            return false;
+        }
+        Accounts other = (Accounts) object;
+        if ((this.login == null && other.login != null) || (this.login != null && !this.login.equals(other.login))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.project2.model.Accounts[ login=" + login + " ]";
+    }
+    
+}
---
+++ src/java/com/project2/model/Fruits.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project2.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author rackarmattan
+ */
+@Entity
+@Table(name = "FRUITS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Fruits.findAll", query = "SELECT f FROM Fruits f")
+    , @NamedQuery(name = "Fruits.findByFname", query = "SELECT f FROM Fruits f WHERE f.fname = :fname")
+    , @NamedQuery(name = "Fruits.findByColor", query = "SELECT f FROM Fruits f WHERE f.color = :color")})
+public class Fruits implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "FNAME")
+    private String fname;
+    @Basic(optional = false)
+    @Column(name = "COLOR")
+    private String color;
+
+    public Fruits() {
+    }
+
+    public Fruits(String fname) {
+        this.fname = fname;
+    }
+
+    public Fruits(String fname, String color) {
+        this.fname = fname;
+        this.color = color;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (fname != null ? fname.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Fruits)) {
+            return false;
+        }
+        Fruits other = (Fruits) object;
+        if ((this.fname == null && other.fname != null) || (this.fname != null && !this.fname.equals(other.fname))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.project2.model.Fruits[ fname=" + fname + " ]";
+    }
+    
+}
