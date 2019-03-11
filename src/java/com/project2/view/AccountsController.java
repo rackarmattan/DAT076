8c26ec3cb66a7c30ddda6f76f87823234091cf56
Matilda Sjöblom <matilda.sjo96@gmail.com>
2019-03-11 08:20:48 +0100

    made accountscontroller requestscoped and fixed resetpass

--- src/java/com/project2/view/AccountsController.java
+++ src/java/com/project2/view/AccountsController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javax.annotation.ManagedBean;
 import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIComponent;
@@ -23,7 +24,7 @@ import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 
 @Named("accountsController")
-@SessionScoped
+@RequestScoped
 public class AccountsController implements Serializable {
 
     private CurrentAccountManager current = CurrentAccountManager.getInstance();
